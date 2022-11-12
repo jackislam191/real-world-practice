c2a63d23aee1c62c1827c920f921147d396b5752
jackislam191 <79713868+jackislam191@users.noreply.github.com>
2022-11-12 22:53:02 +0800

    Add 404 if not item

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,18 +10,26 @@ class PostsController extends Controller
 {
     public function index() {
         $posts = DB::table('posts')
-                    ->limit(10)
                     ->get();
-        // dd($posts);
-        // die('you are in index page');
+        // if (!empty($posts->items)) {
         return Inertia::render('Posts/Listing', ['posts'=>$posts]);
+        // } else {
+            // abort(404);
+        // }
+
     }
 
     public function detail($id) {
         $post = DB::table('posts')
                     ->where('id', '=', $id)
                     ->get();
-        return Inertia::render('Posts/Detail', ['post'=>$post]);
+        // dd($post);
+        if (!empty($post->items)) {
+            return Inertia::render('Posts/Detail', ['post'=>$post]);
+        } else {
+            //should not be 404 , it required error page for no data
+            abort(404);
+        }
     }
 
     public function create() {
